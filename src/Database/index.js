db1d154d985790a4724d9acb248a2fc656b42d26
Rawlinson Rolim <rawlinson.filho@vallourec.com>
2022-08-17 17:57:30 -0300

    ADD: dotenv

--- src/Database/index.js
+++ src/Database/index.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
+require('dotenv').config();
 
 //const uri = "mongodb+srv://rawrolim:CSRR1j47xo68fWMr@cluster1.ftinoos.mongodb.net/contador-horas?retryWrites=true&w=majority";
-mongoose.connect('mongodb://rawrolim:MACAhell2704@localhost:27017/contador-horas', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(''+process.env.URL_MONGO, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
