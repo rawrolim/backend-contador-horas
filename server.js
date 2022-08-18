21ed7e1ef19d842bb60bbc6943ac0c37b7b6f252
Rawlinson Rolim <rawlinson.filho@vallourec.com>
2022-08-18 16:26:35 -0300

    test

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-const app = require('./src/app');;
-const port = process.env.URL || 3000;
+const app = require('./src/app');
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, '0.0.0.0', function () {
     console.log(`app listening on port ${port}`)
