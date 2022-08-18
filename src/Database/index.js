a54a38989bc23a9abcffe66284add169058a79b6
Rawlinson Rolim <rawlinson.filho@vallourec.com>
2022-08-18 08:12:12 -0300

    DONE: retirar URL de mongo PRD

--- src/Database/index.js
+++ src/Database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 require('dotenv').config();
 
-//const uri = "mongodb+srv://rawrolim:CSRR1j47xo68fWMr@cluster1.ftinoos.mongodb.net/contador-horas?retryWrites=true&w=majority";
+const uri = process.env.URL_MONGO_PRD;
 mongoose.connect(''+process.env.URL_MONGO, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = global.Promise;
 
