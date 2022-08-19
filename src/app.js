0c485d9aaca2513aba0550413ae306415a3b624d
Rawlinson Rolim <rawlinson.filho@vallourec.com>
2022-08-19 11:52:29 -0300

    add: cors

--- src/app.js
+++ src/app.js
@@ -4,10 +4,12 @@ const index = require('./Routes/index');
 const api = require('./Routes/api');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
+const cors = require("cors");
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/', index);
